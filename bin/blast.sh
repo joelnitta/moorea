1b72656b50bc044a6960dfe2c75c0e2778d9455f
Joel Nitta <joelnitta@gmail.com>
2015-09-03 15:53:51 -0400

    set up to run blast on new rbcL data

--- bin/blast.sh
+++ bin/blast.sh
@@ -1,4 +1,4 @@
-# BLAST script 2015-08-31
+# BLAST script 2015-09-03
 
 #############
 ### NOTES ###
@@ -14,17 +14,17 @@
 #######################
 
 # rbcL
-# makeblastdb -in rbcl_id.fasta -out rbcL_id.db -dbtype nucl -hash_index
+ makeblastdb -in rbcL_2015-09-03.fasta -out rbcL_id.db -dbtype nucl -hash_index
 
 # trnHpsbA
-makeblastdb -in ~/Analysis/moorea/data/trnHpsbA_2015-08-30.fasta -out trnHpsbA_id.db -dbtype nucl -hash_index
+# makeblastdb -in ~/Analysis/moorea/data/trnHpsbA_2015-08-30.fasta -out trnHpsbA_id.db -dbtype nucl -hash_index
 
 #########################################################
 #### testing the barcode: query library against self ####
 #########################################################
 
-blastn -query rbcL.fasta -db rbcl_test.db -out rbcL_test.xls -outfmt 6 -max_target_seqs 2
-blastn -query trnHpsbA_all.fasta -db trnHpsbA_all.db -out trnHpsbA_all_test.xls -outfmt 6 -max_target_seqs 2
+# blastn -query rbcL.fasta -db rbcl_test.db -out rbcL_test.xls -outfmt 6 -max_target_seqs 2
+# blastn -query trnHpsbA_all.fasta -db trnHpsbA_all.db -out trnHpsbA_all_test.xls -outfmt 6 -max_target_seqs 2
 
 #######################################################################
 #### gameto id: query each unknown fasta sequence against database ####
@@ -33,14 +33,14 @@ blastn -query trnHpsbA_all.fasta -db trnHpsbA_all.db -out trnHpsbA_all_test.xls 
 # save top 2 hits
 # output format is tab separated text
 
-blastn -query plate52rbcL.fasta -db rbcl_id.db -out blast_out_plate52rbcL.txt -outfmt 6 -max_target_seqs 2
+blastn -query rbcL_clean_gametos_2015-09-03.fasta -db rbcl_id.db -out blast_out_rbcL_id_2015-09-03.txt -outfmt 6 -max_target_seqs 2
 
 # try this for short queries that don't show up in match above
-blastn -query gameto_2-4-15.fasta -db rbcl_id.db -out blast_out_gameto_short_2-4-15.txt -outfmt 6 -max_target_seqs 2 -task "blastn-short"
+# blastn -query gameto_2-4-15.fasta -db rbcl_id.db -out blast_out_gameto_short_2-4-15.txt -outfmt 6 -max_target_seqs 2 -task "blastn-short"
 
 # trnHpsbA (check top 2 hits)
-blastn -query ~/Analysis/moorea/data/trnHpsbA_clean_gametos_2015-08-31.fasta -db trnHpsbA_id.db -out blast_out_trnHpsbA_id_08-31-15.txt -outfmt 6 -max_target_seqs 2
+# blastn -query ~/Analysis/moorea/data/trnHpsbA_clean_gametos_2015-08-31.fasta -db trnHpsbA_id.db -out blast_out_trnHpsbA_id_08-31-15.txt -outfmt 6 -max_target_seqs 2
 
 # to add header row to blast output
 # $' \t ' indicates tab
-echo $'query_id\tsubject_id\t%_identity\talignment_length\tmismatches\tgap_opens\tq._start\tq._end\ts._start\ts._end\tevalue\tbit_score' | cat - blast_out_trnHpsbA_id_08-31-15.txt > blast_out_trnHpsbA_id_08-31-15_head.txt
+# echo $'query_id\tsubject_id\t%_identity\talignment_length\tmismatches\tgap_opens\tq._start\tq._end\ts._start\ts._end\tevalue\tbit_score' | cat - blast_out_trnHpsbA_id_08-31-15.txt > blast_out_trnHpsbA_id_08-31-15_head.txt
