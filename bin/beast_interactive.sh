cbf5c046282f974845806e3758a66bd2cfd816c6
Joel Nitta <joelnitta@gmail.com>
2015-09-29 13:58:38 -0400

    setting up beast run

---
+++ bin/beast_interactive.sh
@@ -0,0 +1,10 @@
+# start interactive session (note, increase mem requested to 4gb)
+srun --pty -p interact --mem 4000 -n 2 -N 1 -t 0-12:00 /bin/bash
+
+module load centos6/BEAST-2.1.2
+
+# give beast more memory
+java -Xms1024m -Xmx1024m -jar lib\beast.jar
+
+# run BEAST
+beast -beagle_instances 2 -threads 2 rbcL_broad_2015-09-29_v3.xml
