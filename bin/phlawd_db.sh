0e4872ab032cba33486c8eeb32c1180b2ec12e2c
Joel Nitta <joelnitta@gmail.com>
2015-10-13 10:51:24 -0400

    changed phlawd_db.sh to run on /n/regal/davis_lab/jnitta/genbank

--- bin/phlawd_db.sh
+++ bin/phlawd_db.sh
@@ -3,17 +3,17 @@
 #SBATCH -n 1               			        # Number of cores
 #SBATCH -N 1                			    # Ensure that all cores are on one machine
 #SBATCH -p serial_requeue				    # Partition to submit to
-#SBATCH --mem=4000         			        # Memory pool for all cores. Request 4GB
+#SBATCH --mem=8000         			        # Memory pool for all cores. Request 4GB
 #SBATCH -o phlawd_db.out     			    # File to which STDOUT will be written
 #SBATCH -e phlawd_db.err       			    # File to which STDERR will be written
-#SBATCH -t 1-0:00							# Request 1 day of processing time
+#SBATCH -t 3-0:00							# Request 3 days of processing time
 #SBATCH --mail-type=ALL         			# Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=jnitta@fas.harvard.edu  # Email to which notifications will be sent
 
-# run from Analysis/genbank
+# run from /n/regal/davis_lab/jnitta/genbank
 
 # load phlawd
 module load centos6/phlawd
 
 # run phlawd setupdb on setup file (all plants)
-PHLAWD setupdb ~/Analysis/moorea/bin/2015-10-09/db.setup
+PHLAWD setupdb db.setup
