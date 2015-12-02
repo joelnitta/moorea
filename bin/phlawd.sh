c3b7a666d9e5efe041f09d863a8f1bbcba922b0b
Joel Nitta <joelnitta@gmail.com>
2015-12-02 16:06:32 -0500

    phlawd on matk try 5 (new keep file)

--- bin/phlawd.sh
+++ bin/phlawd.sh
@@ -4,8 +4,8 @@
 #SBATCH -N 1                			    # Ensure that all cores are on one machine
 #SBATCH -p davis				    		# Partition to submit to
 #SBATCH --mem=2000         			        # Memory pool for all cores. Request 2GB
-#SBATCH -o phlawd_db.out     			    # File to which STDOUT will be written
-#SBATCH -e phlawd_db.err       			    # File to which STDERR will be written
+#SBATCH -o phlawd.out     			    # File to which STDOUT will be written
+#SBATCH -e phlawd.err       			    # File to which STDERR will be written
 #SBATCH -t 1-0:00							# Request 1 day of processing time
 #SBATCH --mail-type=ALL         			# Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=jnitta@fas.harvard.edu  # Email to which notifications will be sent
