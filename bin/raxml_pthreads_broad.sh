73d9a08cd41450e388654483b36e19c25c330e6c
Joel Nitta <joelnitta@gmail.com>
2015-12-27 21:46:17 -0500

    changed number of cores from 16 to 8, time from 4 to 7 days

--- bin/raxml_pthreads_broad.sh
+++ bin/raxml_pthreads_broad.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-#SBATCH -n 16                    # Number of cores
+#SBATCH -n 8                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
 #SBATCH -p davis       # Partition to submit to
-#SBATCH --mem=32000               # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=20000               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o raxml.out         # File to which STDOUT will be written
 #SBATCH -e raxml.err         # File to which STDERR will be written
-#SBATCH -t 4-0:00             # run for max 4 days
+#SBATCH -t 7-0:00             # run for max 7 days
 #SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=jnitta@fas.harvard.edu # Email to which notifications will be sent
 
@@ -20,7 +20,7 @@
 module load centos6/RAxML-8.1.15_openmpi-1.6.4_gcc-4.8.0
 
 # compute 20 ML trees, save the best one
-raxmlHPC-PTHREADS -T 16 -m GTRGAMMA -p 12345 -# 20 -s ~/Analysis/moorea/bin/2015-12-23/examl/all_broad.aln.phy -n all_broad_20ML
+raxmlHPC-PTHREADS -T 8 -m GTRGAMMA -p 12345 -# 20 -s all_broad.aln.phy -n all_broad_20ML
 
 # run 100 bootstraps
 # raxmlHPC-PTHREADS -T 16 -m GTRGAMMA -p 12345 -b 12345 -# 100 -s ~/Analysis/moorea/bin/2015-12-02/examl/all_broad.phy -n all_broad_100boot
