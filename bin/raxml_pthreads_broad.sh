30c0852fe805513959875ab84ef6f6fbeaab20d7
Joel Nitta <joelnitta@gmail.com>
2015-12-28 22:03:17 -0500

    changed n cores from 8 to 16

--- bin/raxml_pthreads_broad.sh
+++ bin/raxml_pthreads_broad.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-#SBATCH -n 8                    # Number of cores
+#SBATCH -n 16                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
 #SBATCH -p davis       # Partition to submit to
-#SBATCH --mem=20000               # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=32000               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o raxml.out         # File to which STDOUT will be written
 #SBATCH -e raxml.err         # File to which STDERR will be written
 #SBATCH -t 7-0:00             # run for max 7 days
@@ -19,8 +19,12 @@
 # load RAxML
 module load centos6/RAxML-8.1.15_openmpi-1.6.4_gcc-4.8.0
 
-# compute 20 ML trees, save the best one
-raxmlHPC-PTHREADS -T 8 -m GTRGAMMA -p 12345 -# 20 -s all_broad.aln.phy -n all_broad_20ML
+# Step 1: generate randomized stepwise addition parsimony tree with RAxML using full alignment
+# only doing this to automatically generate "reduced" alignment with identical seqs removed
+raxmlHPC-AVX -y -m GTRCAT -p 12345 -s all_broad.aln.phy -n FullStartingTree
+
+# compute 20 ML trees using reduced alignment, save the best one
+raxmlHPC-PTHREADS -T 8 -m GTRGAMMA -p 12345 -# 20 -s all_broad.aln.phy.reduced -n all_broad_20ML
 
 # run 100 bootstraps
 # raxmlHPC-PTHREADS -T 16 -m GTRGAMMA -p 12345 -b 12345 -# 100 -s ~/Analysis/moorea/bin/2015-12-02/examl/all_broad.phy -n all_broad_100boot
