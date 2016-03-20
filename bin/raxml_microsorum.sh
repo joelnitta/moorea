faf0d06484424db7947732a20641fe2e7d6de734
Joel Nitta <joelnitta@gmail.com>
2016-03-20 08:38:47 -0400

    fixed microsorum pthreads script, added reduced alignment

--- bin/raxml_microsorum.sh
+++ bin/raxml_microsorum.sh
@@ -15,7 +15,7 @@ module load centos6/RAxML-8.1.15_openmpi-1.6.4_gcc-4.8.0
 
 # Step 1: generate randomized stepwise addition parsimony tree with RAxML using full alignment
 # only doing this to automatically generate "reduced" alignment with identical seqs removed
-raxmlHPC-AVX -y -m GTRCAT -p 12345 -s microsorum_plastid.phy -n FullStartingTree
+raxmlHPC-AVX -y -m GTRCAT -p 12345 -s ../../data/microsorum_plastid.phy -n FullStartingTree
 
-# compute 20 ML trees using reduced alignment with 16 cores, save the best one
-raxmlHPC-PTHREADS -T 16 -m GTRGAMMA -p 12345 -N 20 -s all_broad.phy.reduced -n all_broad_20ML
+# compute 100 ML trees using reduced alignment with 16 cores, save the best one
+raxmlHPC-PTHREADS -T 16 -m GTRGAMMA -p 12345 -N 100 -s ../../data/microsorum_plastid.phy.reduced -n microsorum_100ML
