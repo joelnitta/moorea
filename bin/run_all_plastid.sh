b012790c5e8f15a54a90081e6eda650af29a1c26
Joel Nitta <joelnitta@gmail.com>
2017-02-21 20:37:02 +0900

    make simple shell script to submit all plastid raxml scripts

---
+++ bin/run_all_plastid.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ 
+sbatch ../../bin/raxml_microsorum_plastid_both_rogues_in.sh
+sbatch ../../bin/raxml_microsorum_plastid_both_rogues_out.sh
+sbatch ../../bin/raxml_microsorum_plastid_either_rogues_in.sh
+sbatch ../../bin/raxml_microsorum_plastid_either_rogues_out.sh
